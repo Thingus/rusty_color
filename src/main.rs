028a3e509a03e8d92e36b5fe0df81735bae71678
bar <foo>
2020-05-12 22:19:00 +0100

    Can now read frames of samples (after ignorning generics)

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 extern crate hound;
-use hound::{WavReader, WavSpec, WavIntoSamples, WavSamples};
-use sample::{signal, Signal}
+use hound::{WavReader, Sample};
+//use sample::{signal, Signal};
+//use image::{Pixel, Pixels};
 use std::{
-    error::Error,
-    fs::{self, File},
-    io::BufWriter,
-    path::PathBuf,
+//    error::Error,
+//    io::BufWriter,
+//    path::PathBuf,
     string::String,
-    io::Read,
-    any::type_name
+    io::{Read,Seek},
+    any::type_name,
+		error::Error
 };
 
-fn main(){
+fn main() -> Result<(), Box<dyn Error>>{
     //Ask for path to audio file
     let audio_path = String::from("/home/john/projects/rusty_color/sample.wav");
     //Ask for path to output
@@ -21,44 +22,56 @@ fn main(){
     let mut reader = WavReader::open(&audio_path).unwrap();
     let spec = reader.spec();
     
-    
-    //Read whole audio into memory (for now)
-    //This from https://github.com/Michael-F-Bryan/noise-gate/blob/master/examples/wav-splitter.rs
-    
-    let samples = reader
-       .samples::<i32>();
+    let samples = reader.samples::<i32>();
        
     let n_samples = samples.len() as usize;
         
     print!("Sample rate: {}\n", spec.sample_rate);
     print!("Number of samples {}\n", n_samples);
-        
-    print!("Samples are:\n");
-    print_samples(samples)
     
+		let samples = reader
+		.into_samples::<i16>()
+		.map(|result| result.map(|sample| [sample]))
+		.collect::<Result<Vec<_>, _>>()?;
+
+		print!("Window 1 is:");
+		let sample_1 = get_nth_frame(&samples, 0, 20);
+    print!("{}", (&sample_1.len()));
     //Break sample into frames
 				
-
     //Process audio frames to video frames
-    //Put video frames back together
+    
+		//Put video frames back together
     //Save video
+		Ok(())
 }
+
+
+fn get_nth_frame<S>(samples: &Vec<S>, frame_index :usize, frame_length:usize) -> &[S]
+{
+		let frame_start = frame_index * frame_length;
+		let frame_end = frame_start + frame_length;   // Add range check here
+		let mut frame: Vec<S> = Vec::new();
+		&samples[frame_start..frame_end]
+}
+
+
+// fn audio_to_video<S>(
 
 fn type_of<T>(_: T) -> &'static str {
     type_name::<T>()
 }
 
-fn samples_to_frames<R>(samples: WavSamples<R, i32>) -> FrameArray
-where
-
-fn print_samples<R>(samples: WavSamples<R,i32>)
+//fn samples_to_frames<R>(samples: WavSamples<R, i32>) -> FrameArray
+//where
+fn print_samples<S>(samples: Vec<S>)
 where 
-    R: std::io::Read
+		S: Sample
 {
     let mut sample_vaule: i32;
     for this_sample in samples{
         //hound::Sample::read(&mut this_sample.unwrap(), sample_value, 8, 0);
-        print!("{}", this_sample.unwrap());
+        //print!("{:?}", this_sample);
         //print!("{}", type_of(this_sample));
     }
 }
